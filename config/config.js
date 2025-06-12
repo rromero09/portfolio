ce20b91fc5c68d9b6031be49bc2b830647fc8e67
rromero09 <rromero8@neiu.edu>
2025-06-12 13:09:20 -0500

    refactor code for improved readability and fix minor typos in descriptions

--- config/config.js
+++ config/config.js
@@ -1,9 +1,4 @@
 import profile from "./profile.png";
-import {
-  faAppStore,
-  faGithub,
-  faGooglePlay,
-} from "@fortawesome/free-brands-svg-icons";
 
 export const navigation = {
   name: "Rafael Romero",
@@ -21,13 +16,13 @@ export const work = {
     {
       title: "Web Developer | NEIU Student Media Board",
       description:
-        "Worked on  afull-stack Express.js server with form handling and validation. Designed a MongoDB database for CRUD operations",
+        "Worked on a full-stack Express.js server with form handling and validation. Designed a MongoDB database for CRUD operations",
       icons: null,
     },
     {
       title: "Software & Analytics Contributor | The Bakehouse Chicago",
       description:
-        "Created an inventory system in Notion with custom dashboards. Used Square API to automate reporting in Python, increasing revenue and reducing waste. Contributed IT support and helped scale operations from 1 to 4 stores.",
+        "Created an inventory system in Notion with custom dashboards. Used Square API to do reporting  analytics n Python, increasing revenue and reducing waste. Contributed IT support and helped scale operations from 1 to 4 stores.",
       icons: null,
     },
   ],
@@ -55,9 +50,9 @@ export const intro = {
 export const about = {
   title: "Who I Am",
   description: [
-    "I'm a Computer Science new graduate with a focus on backend development and data infrastructure solutions",
+    "I'm a Computer Science new graduate with a focus on backend development and data infrastructure solutions.",
     "My passion lies in backend development, data infrastructure, and building cloud-native applications. I've worked with Spring Boot, FastAPI, Docker, AWS and got recently into AI wrappers such as LangChain and LangGraph.",
-    "Outside tech, I keep myself together through beach volleyball,hikes and exploring new tools that make life (and work) more efficient.",
+    "Outside tech, I keep myself together through beach volleyball, hikes, and exploring new tools that make life (and work) more efficient.",
   ],
 };
 
@@ -68,17 +63,19 @@ export const projects = {
       title: "Inventory Management System",
       description:
         "A lightweight inventory tracking system for The Bakehouse Chicago using Notion + automation to track missing items, reduce waste, and streamline operations.",
+      link: "https://github.com/rromero09",
     },
     {
       title: "Schedule Assistant API",
       description:
-        "An email notification system that sends schedules to employees. Built with FastAPI,Docker and deployed with AWS EC2, it automates schedule management and improves team communication.",
-      icons: [],
+        "An email notification system that sends schedules to employees. Built with FastAPI, Docker and deployed with AWS EC2, it automates schedule management and improves team communication.",
+      link: "https://github.com/rromero09/scheduler",
     },
     {
       title: "Hack Fest 2024 - 2nd Place",
       description:
         "Created a ride-booking assistant for non-English speakers using text-to-speech, React.js, and a Translate API. Recognized for accessibility innovation.",
+      link: "https://github.com/rromero09",
     },
   ],
 };
@@ -98,7 +95,7 @@ export const contact = {
 
 export const SEO = {
   title:
-    "Rafael Romero | Software Developer | Java | Python | Spring Boot| Data Science",
+    "Rafael Romero | Software Developer | Java | Python | Spring Boot | Data Science",
   description:
     "Computer Science graduate specializing in backend development (Spring Boot, FastAPI), data infrastructure, cloud-native tools, and automation. Passionate about solving real-world problems with software.",
   image: profile.src,
@@ -116,6 +113,10 @@ export const links = {
     {
       title: "My GitHub",
       link: "https://github.com/rromero09/",
+    },
+    {
+      title: "Schedule Assistant ",
+      link: "https://github.com/rromero09/scheduler",
     },
   ],
 };
